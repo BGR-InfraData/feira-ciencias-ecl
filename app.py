862533e6d7d2b6c057325a82b4b827626a1b5882
Gustavo Oliveira <gustavo.bibiano@totvs.com.br>
2023-05-28 11:24:10 -0300

    Add Ask Your PDF

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
+import logging
+
 import streamlit as st
 import transformers
 from langchain.chains import LLMChain
+from langchain.chains.question_answering import load_qa_chain
+from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.llms import OpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.prompts import PromptTemplate
+from langchain.text_splitter import CharacterTextSplitter
 from langchain.utilities import WikipediaAPIWrapper
+from langchain.vectorstores import FAISS
+from PyPDF2 import PdfReader
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
 def main():
@@ -13,15 +23,16 @@ def main():
     """
     st.set_page_config(page_title='🔗 INTNEG-GPT', page_icon='🦜')
 
-    add_selectbox = st.sidebar.selectbox(
+    add_selectbox = st.sidebar.radio(
         'Qual modelo quer testar?',
-        ('Youtube GPT Creator', 'Sentiment GPT Analysis', 'PDF GPT Creator', 'CSV GPT Creator'))
+        ('Criador de script para Youtube', 'Análise de Sentimento',
+         'Pergunte ao seu PDF', 'Pergunte ao seu CSV'))
 
-    if add_selectbox == 'Youtube GPT Creator':
+    if add_selectbox == 'Criador de script para Youtube':
 
         wiki = WikipediaAPIWrapper()
 
-        st.title('📹 Youtube GPT Creator')
+        st.title('📹 Criador de script para Youtube')
         prompt = st.text_input(
             'Sobre qual tema quer que eu escreva um script para um vídeo?')
 
@@ -63,7 +74,7 @@ def main():
             with st.expander('Wikipedia Research'):
                 st.info(wiki_research)
 
-    if add_selectbox == 'Sentiment GPT Analysis':
+    if add_selectbox == 'Análise de Sentimento':
 
         # Paper: https://arxiv.org/pdf/2104.12250.pdf
         model_path = "cardiffnlp/twitter-xlm-roberta-base-sentiment"
@@ -73,7 +84,7 @@ def main():
             'negative': 'Negativo',
             'neutral': 'Neutro'
         }
-        st.title('❤️ Sentiment Analysis')
+        st.title('❤️ Análise de Sentimento')
         sa_llm = transformers.pipeline(
             "sentiment-analysis", model=model_path, tokenizer=model_path)
 
@@ -93,6 +104,37 @@ def main():
                 st.write(f'Sentimento: {sentiment}')
             with col2:
                 st.write(f'Score: {round(score*100, 1)}%')
+
+    if add_selectbox == 'Pergunte ao seu PDF':
+
+        st.title('🎨 Pergunte ao seu PDF')
+        pdf = st.file_uploader("Carregue seu PDF", type="pdf")
+
+        if pdf:
+            pdf_reader = PdfReader(pdf)
+            text = ""
+            for page in pdf_reader.pages:
+                text += page.extract_text()
+
+            text_splitter = CharacterTextSplitter(
+                separator="\n",
+                chunk_size=1000,
+                chunk_overlap=200,
+                length_function=len)
+            chunks = text_splitter.split_text(text)
+
+            embeddings = OpenAIEmbeddings()
+            knowledge_base = FAISS.from_texts(chunks, embeddings)
+
+            user_question = st.text_input("Faça uma pergunta sobre o seu PDF:")
+            if user_question:
+                docs = knowledge_base.similarity_search(user_question)
+
+                llm = OpenAI()
+                chain = load_qa_chain(llm, chain_type="stuff")
+                response = chain.run(input_documents=docs,
+                                     question=user_question)
+                st.write(response)
 
 
 if __name__ == '__main__':
